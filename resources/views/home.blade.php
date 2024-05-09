eab4de3d3c68c9b30ff4428d135b589aae525775
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-09 16:11:16 +0200

    [add] replica boolando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,19 @@
     <div class="container">
         <div class="product">
             <img class="visible" src="/img/1.webp" alt="productMainImage">
-            <img src="" alt="">
+            <img src="/img/1b.webp" alt="">
+            <span class="heart d-flex ">&hearts;</span>
+            <span class="discount d-flex"></span>
+            <span v-for="(badge, index) in productbadges" key="index" class="eco d-flex"></span>
+            <p></p>
+            <h3></h3>
+            <span class="prezzo">14,99 &euro;</span>
+            <span class="overline"> &euro;</span>
+        </div>
+
+        <div class="product">
+            <img class="visible" src="/img/2.webp" alt="productMainImage">
+            <img src="/img/2b.webp" alt="">
             <span class="heart d-flex ">&hearts;</span>
             <span class="discount d-flex"></span>
             <span v-for="(badge, index) in productbadges" :key="index" class="eco d-flex"></span>
@@ -17,8 +29,8 @@
         </div>
 
         <div class="product">
-            <img class="visible" src="/img/1.webp" alt="productMainImage">
-            <img src="" alt="">
+            <img class="visible" src="/img/3.webp" alt="productMainImage">
+            <img src="/img/3b.webp" alt="">
             <span class="heart d-flex ">&hearts;</span>
             <span class="discount d-flex"></span>
             <span v-for="(badge, index) in productbadges" :key="index" class="eco d-flex"></span>
@@ -29,8 +41,8 @@
         </div>
 
         <div class="product">
-            <img class="visible" src="/img/1.webp" alt="productMainImage">
-            <img src="" alt="">
+            <img class="visible" src="/img/4.webp" alt="productMainImage">
+            <img src="/img/4b.webp" alt="">
             <span class="heart d-flex ">&hearts;</span>
             <span class="discount d-flex"></span>
             <span v-for="(badge, index) in productbadges" :key="index" class="eco d-flex"></span>
@@ -41,8 +53,8 @@
         </div>
 
         <div class="product">
-            <img class="visible" src="/img/1.webp" alt="productMainImage">
-            <img src="" alt="">
+            <img class="visible" src="/img/5.webp" alt="productMainImage">
+            <img src="/img/5b.webp" alt="">
             <span class="heart d-flex ">&hearts;</span>
             <span class="discount d-flex"></span>
             <span v-for="(badge, index) in productbadges" :key="index" class="eco d-flex"></span>
@@ -53,20 +65,8 @@
         </div>
 
         <div class="product">
-            <img class="visible" src="/img/1.webp" alt="productMainImage">
-            <img src="" alt="">
-            <span class="heart d-flex ">&hearts;</span>
-            <span class="discount d-flex"></span>
-            <span v-for="(badge, index) in productbadges" :key="index" class="eco d-flex"></span>
-            <p></p>
-            <h3></h3>
-            <span class="prezzo">14,99 &euro;</span>
-            <span class="overline"> &euro;</span>
-        </div>
-
-        <div class="product">
-            <img class="visible" src="/img/1.webp" alt="productMainImage">
-            <img src="" alt="">
+            <img class="visible" src="/img/6.webp" alt="productMainImage">
+            <img src="/img/6b.webp" alt="">
             <span class="heart d-flex ">&hearts;</span>
             <span class="discount d-flex"></span>
             <span v-for="(badge, index) in productbadges" :key="index" class="eco d-flex"></span>
